504f01a24671aeaf72ddca507a15ea01172f6320
Euan Coyle <92370561+Ecoyle33@users.noreply.github.com>
2023-12-15 18:49:55 +0000

    final changes

--- task 3/tast_3_pt2.py
+++ task 3/tast_3_pt2.py
@@ -1,4 +1,4 @@
-with open("t3.txt") as file:
+with open("C:\\Users\\euanc\\OneDrive\\Advent Coding Challenge\\Advent Coding Challenge Answers\\Advent-Of-Code-2023-Answers\\task 3\\t3.txt") as file:
     # read the file text into a variable
     data = file.read()
 
@@ -11,11 +11,16 @@ n = len(lines)
 # length of line of text, all the lines are equal length
 m = len(lines[0])
 
+gear_numbers = [[[] for _ in range(m)] for _ in range(n)]
+
 # Iterates through each element in a line of text, returning true if the 
 # character iterated over is a symbol
-def is_symbol(row, col):
+def is_symbol(row, col, num):
     if not (0 <= row < n and 0 <= col < m):
         return False
+    
+    if lines[row][col] == "*":
+        gear_numbers[row][col].append(num)
 
     return lines[row][col] != "." and not lines[row][col].isdigit()
 
@@ -42,13 +47,15 @@ for row, line in enumerate(lines):
         num = int(num)
 
         # number ended, look around
-        if is_symbol(row, start - 1) or is_symbol(row, col):
-            ans += num
-            continue
+        is_symbol(row, start - 1, num) or is_symbol(row, col, num)
+
 
         for k in range(start - 1, col + 1):
-            if is_symbol(row - 1, k) or is_symbol(row + 1, k):
-                ans += num
-                break
+            is_symbol(row - 1, k, num) or is_symbol(row + 1, k, num)
 
+for i in range(n):
+    for j in range(m):
+        nums = gear_numbers[i][j]
+        if lines[i][j] == "*" and len(nums) == 2:
+            ans += nums[0] * nums[1]
 print(ans)
