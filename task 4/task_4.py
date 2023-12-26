e9050f36be47bc312658b19b2e39e75e4f596a83
Euan Coyle <92370561+Ecoyle33@users.noreply.github.com>
2023-12-26 15:46:36 +0000

    input + pseudo code

---
+++ task 4/task_4.py
@@ -0,0 +1,13 @@
+# pseudo code:
+# read the data into the program
+# remove the key from the string and split the string into two variables
+# first variable is a set containing the 'winning numbers'
+# second variable contains all the 'numbers you have'
+# create two more variables storing the current and total scores of the cards
+# create a final variable keeping track of the number of winning numbers (wn_count) in the second variable
+# iterate through the 'numbers you have', checking each time if they match one of the winning numbers
+# if they match and wn_count == 0, add 1 to current score and add 1 to wn_count
+# if they match and wn_count >= 1, double the current score and add 1 to wn_count
+# once you've iterated through the numbers you have, add the final score from the current card to the total score
+# repeat the previous steps for each card in the pile
+# return the total score 
